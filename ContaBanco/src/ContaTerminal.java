3ca4ac2a02b01cb011faea0ce62fc6749cf02622
denis.menegon <denismenegon@hotmail.com>
2024-09-27 18:52:05 -0300

    Exercício Finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Informe seu Nome?");
+        String strNome = s.nextLine();
+
+        System.out.println("Digite sua Agência!?");
+        String strAgencia = s.nextLine();
+
+        System.out.println("Digite sua Conta!?");
+        int intConta = s.nextInt();
+
+        System.out.println("Informe seu Saldo Atual?");
+        double dblSaldo = s.nextDouble();
+
+        s.close();
+
+        System.out.println("Olá [" + strNome + "], obrigado por criar uma conta em nosso banco, sua agência é [" + strAgencia + "], conta [" + intConta + "] e seu saldo [" + dblSaldo + "] já está disponível para saque.");
     }
 }
